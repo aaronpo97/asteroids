bf7ec622d2c99336ea7683fa1bf6779f3493fdf3
Aaron William Po <apo2@uwo.ca>
2024-01-23 21:07:48 -0500

    Add asteroids and projectile collision detection

--- index.js
+++ index.js
@@ -35,9 +35,12 @@ class Player {
     c.translate(this.position.x, this.position.y);
     c.rotate(this.rotation);
     c.translate(-this.position.x, -this.position.y);
+
+    c.beginPath();
     c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
     c.fillStyle = 'red';
     c.fill();
+    c.closePath();
 
     c.beginPath();
     c.moveTo(this.position.x + 30, this.position.y);
@@ -56,6 +59,8 @@ class Player {
     this.position.y += this.velocity.y;
   }
 }
+
+// todo - when transitioning to typescript make a generic Ciarcle class for projectiles and asteroids
 
 class Projectile {
   /**
@@ -86,6 +91,36 @@ class Projectile {
   }
 }
 
+class Asteroid {
+  /**
+   * @param {{
+   *   position: { x: number; y: number };
+   *   velocity: { x: number; y: number };
+   *   radius: number;
+   * }} args
+   */
+  constructor({ position, velocity, radius }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = radius;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    c.closePath();
+
+    c.strokeStyle = 'white';
+    c.stroke();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 const player = new Player({
   position: { x: canvas.width / 2, y: canvas.height / 2 },
   velocity: { x: 0, y: 0 },
@@ -102,6 +137,82 @@ const ROTATIONAL_SPEED = 0.05;
 const FRICTION = 0.98;
 const PROJECTILE_SPEED = 10;
 const projectiles = [];
+const asteroids = [];
+
+// spawn asteroids
+window.setInterval(() => {
+  const index = Math.floor(Math.random() * 4);
+
+  let x, y;
+  let vx, vy;
+  let radius = 50 * Math.random() + 10;
+  switch (index) {
+    case 0: // left side of the screen
+      x = 0 - radius;
+      y = Math.random() * canvas.height;
+      vx = 1;
+      vy = 0;
+      break;
+    case 1: // bottom side of the screen
+      x = Math.random() * canvas.width;
+      y = canvas.height + radius;
+      vx = 0;
+      vy = -1;
+      break;
+    case 2: // right side of the screen
+      x = canvas.width + radius;
+      y = Math.random() * canvas.height;
+      vx = -1;
+      vy = 0;
+      break;
+    case 3: // top side of the screen
+      x = Math.random() * canvas.width;
+      y = 0 - radius;
+      vx = 0;
+      vy = 1;
+      break;
+  }
+
+  asteroids.push(
+    new Asteroid({
+      position: {
+        x: x,
+        y: y,
+      },
+      velocity: {
+        x: vx,
+        y: vy,
+      },
+      radius,
+    }),
+  );
+}, 3000);
+
+/**
+ * @param {{
+ *   position: { x: number; y: number };
+ *   radius: number;
+ * }} circle1
+ * @param {{
+ *   position: { x: number; y: number };
+ *   radius: number;
+ * }} circle2
+ * @returns
+ */
+function circleCollision(circle1, circle2) {
+  const xDiff = circle2.position.x - circle1.position.x;
+  const yDiff = circle2.position.y - circle1.position.y;
+
+  // using the Pythagorean theorem to calculate the distance between the two circles
+  const distance = Math.sqrt(xDiff ** 2 + yDiff ** 2);
+
+  // if the distance is less than the sum of the two radii, then the circles are colliding
+  if (distance <= circle1.radius + circle2.radius) {
+    return true;
+  }
+
+  return false;
+}
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -124,6 +235,34 @@ function animate() {
     }
   }
 
+  // asteroid management
+  for (let i = asteroids.length - 1; i >= 0; i -= 1) {
+    const asteroid = asteroids[i];
+    asteroid.update();
+
+    // projectiles
+
+    if (
+      asteroid.position.x + asteroid.radius < 0 ||
+      asteroid.position.x - asteroid.radius > canvas.width ||
+      asteroid.position.y - asteroid.radius > canvas.height ||
+      asteroid.position.y + asteroid.radius < 0
+    ) {
+      asteroids.splice(i, 1);
+    }
+
+    for (let j = projectiles.length - 1; j >= 0; j -= 1) {
+      const projectile = projectiles[j];
+
+      // todo: make the asteroids smaller when they are hit if they are big enough
+      if (circleCollision(asteroid, projectile)) {
+        asteroids.splice(i, 1);
+        projectiles.splice(j, 1);
+        break;
+      }
+    }
+  }
+
   if (keys.w.pressed) {
     player.velocity.x = Math.cos(player.rotation) * SPEED;
     player.velocity.y = Math.sin(player.rotation) * SPEED;
@@ -131,6 +270,7 @@ function animate() {
     player.velocity.x *= FRICTION;
     player.velocity.y *= FRICTION;
   }
+
   if (keys.d.pressed) {
     player.rotation += ROTATIONAL_SPEED;
   } else if (keys.a.pressed) {
@@ -142,17 +282,18 @@ animate();
 
 window.addEventListener('keydown', (e) => {
   switch (e.code) {
-    case 'KeyW':
+    case 'KeyW': {
       keys.w.pressed = true;
       break;
-    case 'KeyA':
+    }
+    case 'KeyA': {
       keys.a.pressed = true;
       break;
-
-    case 'KeyD':
+    }
+    case 'KeyD': {
       keys.d.pressed = true;
       break;
-
+    }
     case 'Space': {
       projectiles.push(
         new Projectile({
@@ -182,8 +323,12 @@ window.addEventListener('keyup', (e) => {
     case 'KeyD':
       keys.d.pressed = false;
       break;
-
     default:
       break;
   }
+});
+
+window.addEventListener('resize', () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
 });
