9a6e82fbe73d18303e6492592e928af38f07c0d7
Aaron William Po <apo2@uwo.ca>
2024-01-26 23:06:11 -0500

    update asteroid spawn to come in on angles

--- src/spawnHelpers.ts
+++ src/spawnHelpers.ts
@@ -2,48 +2,57 @@ import Asteroid from './classes/Asteroid';
 import type GameState from './classes/GameState';
 import type { Vector } from './types';
 
-export const spawnAsteroid = ({
-  canvas,
-  c,
-  state,
-}: {
+interface SpawnAsteroidArgs {
   canvas: HTMLCanvasElement;
   c: CanvasRenderingContext2D;
   state: GameState;
-}) => {
-  const index = Math.floor(Math.random() * 4);
-
+}
+export const spawnAsteroid = ({ canvas, c, state }: SpawnAsteroidArgs) => {
+  const positions = ['left', 'bottom', 'right', 'top'] as const;
+  const randomPosition = positions[Math.floor(Math.random() * positions.length)];
   const position: Vector = { x: 0, y: 0 };
   const velocity: Vector = { x: 0, y: 0 };
-
-  const radius = 50 * Math.random() + 10;
-
-  switch (index) {
-    case 0: // left side of the screen
+  const radius = 50;
+  switch (randomPosition) {
+    case 'left': {
+      // 0 to PI radians (0 to 180 degrees)
+      const angle = Math.random() * Math.PI;
       position.x = 0 - radius;
       position.y = Math.random() * canvas.height;
-      velocity.x = 1;
-      velocity.y = 0;
+
+      velocity.x = Math.sin(angle);
+      velocity.y = Math.cos(angle);
       break;
-    case 1: // bottom side of the screen
+    }
+    case 'bottom': {
+      // 90 to 270 degrees
+      const angle = Math.random() * Math.PI + Math.PI / 2;
       position.x = Math.random() * canvas.width;
       position.y = canvas.height + radius;
-      velocity.x = 0;
-      velocity.y = -1;
+      velocity.x = Math.sin(angle);
+      velocity.y = Math.cos(angle);
       break;
-    case 2: // right side of the screen
+    }
+    case 'right': {
+      // 180 to 360 degrees
+      const angle = Math.random() * Math.PI + Math.PI;
       position.x = canvas.width + radius;
       position.y = Math.random() * canvas.height;
-      velocity.x = -1;
-      velocity.y = 0;
 
+      velocity.x = Math.sin(angle);
+      velocity.y = Math.cos(angle);
       break;
-    case 3: // top side of the screen
+    }
+
+    case 'top': {
+      // 270 to 450 degrees
+      const angle = Math.random() * Math.PI + (Math.PI * 3) / 2;
       position.x = Math.random() * canvas.width;
       position.y = 0 - radius;
-      velocity.x = 0;
-      velocity.y = 1;
+      velocity.x = Math.sin(angle);
+      velocity.y = Math.cos(angle);
       break;
+    }
     default:
       break;
   }
